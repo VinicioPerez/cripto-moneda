d01ee72eeec68cf8eafb38dcd522677c01a05387
Marco Perez <108315533+VinicioPerez@users.noreply.github.com>
2024-07-31 17:08:02 -0500

    terminando el proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, reactive } from 'vue'
+  import Alerta from './components/Alerta.vue';
+  import Spinner from './components/Spinner.vue';
+  import Cotizacion from './components/Cotizacion.vue';
+  import useCripto from './composables/useCripto';
 
-  const monedas = ref([
-      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
-      { codigo: 'MXN', texto: 'Peso Mexicano'},
-      { codigo: 'EUR', texto: 'Euro'},
-      { codigo: 'GBP', texto: 'Libra Esterlina'},
-  ])
+  const { 
+    monedas, 
+    criptomonedas, 
+    cargando,
+    cotizacion,
+    obtenerCotizacion,
+    mostrarResultado
+  } = useCripto()
+  
+  const error = ref('')
+  const cotizar = reactive({
+    moneda: '',
+    criptomoneda: '',
+  })  
 
+  const cotizarCripto = () => {
+    //Validar que los campos contengan algo 
+    if (Object.values(cotizar).includes('')){
+      error.value = 'Todos los campos son obligatorios'
+      return
+    }
+    error.value = ''
+    obtenerCotizacion(cotizar)
+  }
 
 </script>
 
@@ -15,10 +36,20 @@
   <div class="contenedor">
     <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
     <div class="contenido">
-      <form class="formulario">
+      <Alerta
+        v-if="error"
+      >{{ error }}</Alerta>
+      <form 
+        class="formulario"
+        @submit.prevent="cotizarCripto"
+      >
+
         <div class="campo">
           <label for="moneda">Moneda:</label>
-          <select name="" id="moneda">
+          <select 
+            id="moneda"
+            v-model="cotizar.moneda"
+          >
             <option value="">-- Selecciona --</option>
             <option 
               v-for="moneda in monedas" 
@@ -27,7 +58,28 @@
             </option>
           </select>
         </div>
+
+        <div class="campo">
+          <label for="cripto">Criptomoneda:</label>
+          <select 
+            id="cripto"
+            v-model="cotizar.criptomoneda"
+          >
+            <option value="">-- Selecciona --</option>
+            <option 
+              v-for="criptomoneda in criptomonedas" 
+              :value="criptomoneda.CoinInfo.Name"
+            >{{ criptomoneda.CoinInfo.FullName }}
+            </option>
+          </select>
+        </div>
+        <input 
+          type="submit" 
+          value="Cotizar"
+        />
       </form>
+      <Spinner v-if="cargando"/>
+      <Cotizacion v-if="mostrarResultado" :cotizacion="cotizacion" />      
     </div>
   </div>
  
---
+++ src/components/Alerta.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="alerta">
+        <slot></slot>
+    </div>
+</template>
+
+<style>
+    .alerta {
+        text-align: center;
+        margin-bottom: 2rem;
+        color: var(--blanco);
+        background-color: rgb(214, 19, 19);
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 1.4rem;
+        padding: 1rem;
+        border-radius: 1.5rem;
+    }
+</style>
